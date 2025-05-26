aed11194142f872b41de412a9ebd49b0911965ac
Lucia Predescu <luciana-579@hotmail.com>
2025-05-26 23:14:28 +0200

    initial commit

---
+++ src/test/java/com/example/OrdforandeLista/OrdforandeListaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrdforandeLista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdforandeListaApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
